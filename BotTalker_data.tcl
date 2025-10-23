cbe1ae62137cf2d917e94537be8e353bb5b64f57
Ginzu78 <ginzundu@gmail.com>
2025-10-23 17:17:06 +0900

    BotTalker_data.tcl

---
+++ BotTalker_data.tcl
@@ -0,0 +1,15 @@
+# Bot Talker Data file.
+set TalkzStrArray {
+ {Hey pplz}
+ {Greetings}
+ {love is love}
+ {Never ask me whay}
+ {Sayonara}
+ {how are you?}
+ {that's not a problem}
+ {is it possible?}
+ {Every strike brings me closer to the next home run>
+Indonesi: Setiap serangan membawa saya semakin dekat>
+ {Believe you can and you're halfway there. by - The>
+Indonesia: Yakinlah kamu bisa dan kamu sudah setenga>
+}
